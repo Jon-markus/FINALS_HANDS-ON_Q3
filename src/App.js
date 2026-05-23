956733588f86a10b885ae954f5afc221afaa578b
John Marc De Villa <hellloworld.205@gmail.com>
2026-05-23 21:13:36 +0800

    update push origin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask } from "./features/TaskSlice";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+
+  const dispatch = useDispatch();
+
+  const tasks = useSelector(
+    (state) => state.tasks.taskList
+  );
+
+  const handleAddTask = () => {
+  
+    if (task.trim() === "") {
+      alert("Task cannot be empty!");
+      return;
+    }
+
+    dispatch(addTask(task));
+    setTask("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Task Manager App</h1>
+
+      <div className="input-section">
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={task}
+          onChange={(e) =>
+            setTask(e.target.value)
+          }
+        />
+
+        <button onClick={handleAddTask}>
+          Add Task
+        </button>
+      </div>
+
+      <h2>Task List</h2>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
